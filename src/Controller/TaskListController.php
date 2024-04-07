bbc19736a796f48ce44787ace2a698af06022517
Sanjeev <sharmag77022@gmail.com>
2024-04-07 18:33:45 +0530

    deleting tasks

--- src/Controller/TaskListController.php
+++ src/Controller/TaskListController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Task;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,8 +13,22 @@ class TaskListController extends AbstractController
     #[Route('/task-list', name: 'app_task_list')]
     public function index(EntityManagerInterface $entityManager): Response
     {
+        $tasks = $entityManager->getRepository(Task::class)->findAll();
         return $this->render('task_list/index.html.twig', [
-            'controller_name' => 'TaskListController',
+            'tasks' => $tasks,
         ]);
+    }
+    #[Route('/taskCompleted/{id}', name: 'app_task_complete')]
+    public function taskCompleted($id,EntityManagerInterface $entityManager): Response
+    {
+        $task = $entityManager->getRepository(Task::class)->find($id);
+        if (!$task) {
+            throw $this->createNotFoundException(
+                'No task found'
+            );
+        }
+        $entityManager->remove($task);
+        $entityManager->flush();
+        return $this->redirectToRoute('app_task_list');
     }
 }
